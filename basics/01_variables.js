b9a19421813659af3076e00447be21f198671c90
Rohan-2121 <carslover2130@gmail.com>
2025-02-12 09:33:31 +0000

    varibales..

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const accountId = 12345
+var accountName = "Rohan"
+let accountGmail = "abc@gmail.com"
+accountName = "Pratik"
+
+console.log(accountId);
+console.table([accountId, accountGmail, accountName])
+
